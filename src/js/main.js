e8b54f8e21a72a34dc01479cf80ac06d9be2d48d
Krzysztof Piekarz <kpiekarz1983@gmail.com>
2018-06-12 20:13:42 +0200

    Finished 15.6 advanced

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,19 @@
+import '../sass/main.scss';
+
+let startButton = document.getElementById('start'),
+    stopButton = document.getElementById('stop'),
+    resetButton = document.getElementById('reset'),
+    addButton = document.getElementById('add'),
+    clearButton = document.getElementById('clear'),
+    display = document.querySelector('.stopwatch'),
+    table = document.querySelector('.table'),
+    resultsTable = document.querySelector('.results');
+
+clearButton.setAttribute('disabled', 'disabled');
+
 class Stopwatch {
-    constructor(display) {
+    constructor() {
         this.running = false;
-        this.display = display;
         this.reset();
         this.print(this.times);
     }
@@ -13,7 +25,7 @@ class Stopwatch {
         };
     }
     print() {
-        this.display.innerText = this.format(this.times);
+        display.innerText = this.format(this.times);
     }
     format() {
         return `${pad0(this.times.minutes)}:${pad0(this.times.seconds)}:${pad0(Math.floor(this.times.miliseconds))}`;
@@ -49,6 +61,34 @@ class Stopwatch {
         this.reset();
         this.print(this.times);
     }
+    addToResults() {
+        let results = resultsTable.querySelectorAll('li').length + 1,
+            lastResult = this.format(this.times),
+            li = document.createElement('li'),
+            paragraph = document.createElement('p');
+
+        li.innerText = `${results}. ${lastResult}`;
+        li.setAttribute('class', 'list-group-item');
+
+        paragraph.innerText = 'Osiągnięto maksymalną liczbę wyników dostępną dla tabeli!';
+
+        resultsTable.appendChild(li);
+
+        if (results >= 10) {
+            addButton.setAttribute('disabled', 'disabled');
+            table.insertBefore(paragraph, clearButton);
+        }
+
+        if (clearButton.getAttribute('disabled') === 'disabled') {
+            clearButton.removeAttribute('disabled');
+        }
+    }
+    clearResults() {
+        resultsTable.innerHTML = '';
+        clearButton.setAttribute('disabled', 'disabled');
+        table.removeChild(table.querySelector('p'));
+        addButton.removeAttribute('disabled');
+    }
 }
 
 function pad0 (value) {
@@ -59,11 +99,10 @@ function pad0 (value) {
     return result;
 }
 
-const stopwatch = new Stopwatch(document.querySelector('.stopwatch'), document.querySelector('.results'));
-let startButton = document.getElementById('start');
-let stopButton = document.getElementById('stop');
-let resetButton = document.getElementById('reset');
+const stopwatch = new Stopwatch();
 
 startButton.addEventListener('click', () => stopwatch.start());
 stopButton.addEventListener('click', () => stopwatch.stop());
 resetButton.addEventListener('click', () => stopwatch.resetTimer());
+addButton.addEventListener('click', () => stopwatch.addToResults());
+clearButton.addEventListener('click', () => stopwatch.clearResults());
